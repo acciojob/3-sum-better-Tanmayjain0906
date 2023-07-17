1fd5c330b400a5f8428e2a37d08d53063f248510
Tanmay jain <134151751+Tanmayjain0906@users.noreply.github.com>
2023-07-17 16:53:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-function threeSum(arr, target) {
-    let findIndex;
-    for(let i=0;i<arr.length;i++)
-    {
-        if(arr[i] === target)
-        {
-            findIndex = i;
+unction threeSum(arr, target) {
+// write your code here
+	 arr.sort((a, b) => {
+          return a - b;
+        });
+
+        let closestSum = arr[0] + arr[1] + arr[2];
+
+        //Start from i = 1 cause first three sums are already taken care of
+        for (let i = 1; i < arr.length - 2; i++) {
+          let j = i + 1;
+          let k = arr.length - 1;
+        
+          //Using two Pointers approach 
+          while (j < k) {
+            let sum = arr[i] + arr[j] + arr[k];
+
+			//Numbers can be negative so use abs to get the closest number
+            if (Math.abs(closestSum - target) > Math.abs(sum - target)) {
+              closestSum = sum;
+            }
+
+            if (sum < target) {
+              j++;
+            } else {
+              k--;
+            }
+          }
         }
-    }
-    arr.splice(findIndex+1);
-    let ans = arr.reduce((h1, h2) => {
-        return h1 + h2;
-    })
-    return ans;
+	return closestSum;
 }
 
 module.exports = threeSum;
